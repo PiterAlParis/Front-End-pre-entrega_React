29ffea038fc6410ad287a7f13d063c4b9376f5b4
Lean <166943015+PiterAlParis@users.noreply.github.com>
2026-05-22 10:56:28 -0300

    Update contact information and image imports

--- src/components/Trajetas/Directorio.jsx
+++ src/components/Trajetas/Directorio.jsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { TarjetaContacto } from "./TarjetaContacto";
 import "./Directorio.css";
 import { useTitle } from "../../hooks/useTitle";
+import Adulto1 from "../../assets/img/Adulto_01.jpg";
+import Adulto2 from "../../assets/img/Adulto_02.jpg";
+import Adulto3 from "../../assets/img/Adulto_03.jpg";
 
 const DEFAULT_NOSOTROS = [
-  { id: 1, nombre: "Ana Pérez", email: "ana@example.com", puesto: "CEO", foto: "/src/assets/img/Adulto_01.jpg" },
-  { id: 2, nombre: "Luis Gómez", email: "luis@example.com", puesto: "Dev", foto: "/src/assets/img/Adulto_02.jpg" },
-  { id: 3, nombre: "María Ruiz", email: "maria@example.com", puesto: "Diseño", foto: "/src/assets/img/Adulto_03.jpg" },
+  { id: 1, nombre: "Ana Pérez", email: "aperez@mi-tienda.com", puesto: "CEO", foto: Adulto1 },
+  { id: 2, nombre: "Luis Gómez", email: "lgomez@mi-tienda.com", puesto: "Dev", foto: Adulto2 },
+  { id: 3, nombre: "Leandro Alvarez Parisella", email: "lalvarezparisella@mi-tienda.com", puesto: "Diseño", foto: Adulto3 },
 ];
 
 export const Directorio = () => {
