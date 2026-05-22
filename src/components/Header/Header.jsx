588be32d03b0f50bbc30e72bf08e7d2a006ec8be
Lean <166943015+PiterAlParis@users.noreply.github.com>
2026-05-22 12:01:18 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Nav } from "../../Nav/Nav";
 import "./Header.css";
-import "../../assets/logo.png";
+import logoPatitas from "../../assets/logo.png";
 
 export default function Header() {
 	return (
 		<header className="header">
 			<div className="header-brand">
-				<img src="/logo.png" alt="Logo Patitas" className="header-logo" />
+				<img src=¨{logoPatistas} alt="Logo Patitas" className="header-logo" />
 				<h1>Patistas</h1>
 			</div>
 			<Nav />
