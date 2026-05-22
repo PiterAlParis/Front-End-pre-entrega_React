139ec961b6f55a0c73c64d1546fab3c523ed1132
Lean <166943015+PiterAlParis@users.noreply.github.com>
2026-05-22 11:51:34 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
 	return (
 		<header className="header">
 			<div className="header-brand">
-				<img src="/logo.png" alt="Logo Mi tienda" className="header-logo" />
+				<img src="/logo.png" alt="Logo Patitas" className="header-logo" />
 				<h1>Patistas</h1>
 			</div>
 			<Nav />
