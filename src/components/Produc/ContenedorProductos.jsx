9b775c7f7c91291b9dca61442c9c71ac8d681ec0
Lean <166943015+PiterAlParis@users.noreply.github.com>
2026-05-22 10:57:31 -0300

    Update ContenedorProductos.jsx

--- src/components/Produc/ContenedorProductos.jsx
+++ src/components/Produc/ContenedorProductos.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { useCart } from "../../hooks/useCart";
 import { ListaProductos } from "./ListaProductos";
+import Adulto1 from "../../assets/img/Adulto_01.jpg";
+import Adulto2 from "../../assets/img/Adulto_02.jpg";
+import Adulto3 from "../../assets/img/Adulto_03.jpg";
 
 export const ContenedorProductos = () => {
   const productosArray = [
-    { id: 1, nombre: "Mini Akita", descripcion: "descripcion 1", precio: 123456, img: "/src/assets/img/Adulto_01.jpg" },
-    { id: 2, nombre: "Border Collie", descripcion: "descripcion 2", precio: 1654, img: "/src/assets/img/Adulto_02.jpg" },
-    { id: 3, nombre: "Pastor Australiano", descripcion: "descripcion 3", precio: 4221, img: "/src/assets/img/Adulto_03.jpg" },
+    { id: 1, nombre: "Mini Akita", descripcion: "descripcion 1", precio: 123456, img: Adulto1 },
+    { id: 2, nombre: "Border Collie", descripcion: "descripcion 2", precio: 1654, img: Adulto2 },
+    { id: 3, nombre: "Pastor Australiano", descripcion: "descripcion 3", precio: 4221, img: Adulto3 },
   ];
-
   const [productos] = useState(productosArray);
   const { count } = useCart();
 
